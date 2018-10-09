ee7a66e210790e288b474a72a822aef086370bce
Arnau Siches <asiches@gmail.com>
2018-10-09 11:53:02 +0100

    feat: Seal from bytes
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- src/seal.rs
+++ src/seal.rs
@@ -59,18 +59,72 @@ impl Seal {
         result
     }
 
-    pub fn from_str(raw: &str) -> Result<Seal, SealError> {
-        if !raw.starts_with("**REDACTED**") {
+    /// Creates a `Seal` from a string. The string must have either the Objecthash prefix
+    /// `**REDACTED**` or the blot [`SEAL_MARK`].
+    ///
+    /// You can use [`from_bytes`] if you have a list of bytes.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # extern crate blot;
+    /// # use blot::seal::Seal;
+    /// let seal_classic = Seal::from_str("**REDACTED**1220a6a6e5e783c363cd95693ec189c2682315d956869397738679b56305f2095038");
+    /// let seal = Seal::from_str("771220a6a6e5e783c363cd95693ec189c2682315d956869397738679b56305f2095038");
+    ///
+    /// assert!(seal_classic.is_ok());
+    /// assert!(seal.is_ok());
+    /// assert_eq!(seal.unwrap(), seal_classic.unwrap());
+    /// ```
+    pub fn from_str(input: &str) -> Result<Seal, SealError> {
+        let bare = if input.starts_with("**REDACTED**") {
+            input
+                .get(12..)
+                .expect("Expected a redacted hash starting with `**REDACTED**`")
+        } else if input.starts_with("77") {
+            input
+                .get(2..)
+                .expect("Expected a redacted hash starting with `0x77`")
+        } else {
+            return Err(SealError::NotRedacted);
+        };
+
+        let bytes = Vec::from_hex(bare)?;
+
+        Seal::from_bytes_without_mark(&bytes)
+    }
+
+    /// Creates a `Seal` from a list of bytes. The first byte must be the
+    /// [`SEAL_MARK`].
+    ///
+    /// You can use [`from_str`] if your redacted hash uses the original Objecthash `"**REDACTED**"` prefix.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # extern crate hex;
+    /// # extern crate blot;
+    /// # use blot::seal::Seal;
+    /// # use hex::FromHex;
+    /// let bytes = Vec::from_hex("771220a6a6e5e783c363cd95693ec189c2682315d956869397738679b56305f2095038").unwrap();
+    /// let seal = Seal::from_bytes(&bytes);
+    ///
+    /// assert!(seal.is_ok());
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// This operation fails with [`SealError::NotRedacted`] if the first byte is not `0x77`, the
+    /// seal mark.
+    pub fn from_bytes(bytes: &[u8]) -> Result<Seal, SealError> {
+        if bytes[0] != SEAL_MARK {
             return Err(SealError::NotRedacted);
         }
 
-        let bytes = Vec::from_hex(raw.get(12..).expect("REDACTED"))?;
-
-        // TODO: Check 0x77
-        Seal::from_bytes(&bytes)
+        Seal::from_bytes_without_mark(&bytes[1..])
     }
 
-    pub fn from_bytes(bytes: &[u8]) -> Result<Seal, SealError> {
+    fn from_bytes_without_mark(bytes: &[u8]) -> Result<Seal, SealError> {
         let (code, rest) = Uvar::take(&bytes)?;
         let tag: Result<Tag, _> = code.into();
 
