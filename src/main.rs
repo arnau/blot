8158db1bfaccbe00ddd327f8c4ad30d33e9018fd
Arnau Siches <asiches@gmail.com>
2018-10-17 13:30:30 +0100

    feat: Consume stdin when input is not provided
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use ansi_term::Colour::{Black, Fixed};
 use blot::core::Blot;
 use blot::multihash::{self, Hash, Multihash};
 use blot::value::{Value, ValueSet};
+use std::io::{self, Read};
 
 use clap::{App, Arg};
 
@@ -29,7 +30,7 @@ fn main() {
                     r#"
 For example, "foo", {"foo": "bar"}, [1, "foo"]
                 "#,
-                ).required(true)
+                )
                 .index(1),
         ).arg(
             Arg::with_name("algorithm")
@@ -62,29 +63,50 @@ For example, "foo", {"foo": "bar"}, [1, "foo"]
                 .long("verbose"),
         ).get_matches();
 
-    let input = matches.value_of("input").unwrap();
+    let input = matches
+        .value_of("input")
+        .map(handle_stdin)
+        .unwrap_or_else(|| consume_stdin());
     let seq_mode = matches.value_of("sequence").unwrap();
     let verbose = matches.is_present("verbose");
 
     match matches.value_of("algorithm").unwrap() {
-        "sha1" => digest_command(input, seq_mode, verbose, multihash::Sha1),
-        "sha2-256" => digest_command(input, seq_mode, verbose, multihash::Sha2256),
-        "sha2-512" => digest_command(input, seq_mode, verbose, multihash::Sha2512),
-        "sha3-224" => digest_command(input, seq_mode, verbose, multihash::Sha3224),
-        "sha3-256" => digest_command(input, seq_mode, verbose, multihash::Sha3256),
-        "sha3-384" => digest_command(input, seq_mode, verbose, multihash::Sha3384),
-        "sha3-512" => digest_command(input, seq_mode, verbose, multihash::Sha3512),
-        "blake2b-512" => digest_command(input, seq_mode, verbose, multihash::Blake2b512),
-        "blake2s-256" => digest_command(input, seq_mode, verbose, multihash::Blake2s256),
+        "sha1" => digest_command(&input, seq_mode, verbose, multihash::Sha1),
+        "sha2-256" => digest_command(&input, seq_mode, verbose, multihash::Sha2256),
+        "sha2-512" => digest_command(&input, seq_mode, verbose, multihash::Sha2512),
+        "sha3-224" => digest_command(&input, seq_mode, verbose, multihash::Sha3224),
+        "sha3-256" => digest_command(&input, seq_mode, verbose, multihash::Sha3256),
+        "sha3-384" => digest_command(&input, seq_mode, verbose, multihash::Sha3384),
+        "sha3-512" => digest_command(&input, seq_mode, verbose, multihash::Sha3512),
+        "blake2b-512" => digest_command(&input, seq_mode, verbose, multihash::Blake2b512),
+        "blake2s-256" => digest_command(&input, seq_mode, verbose, multihash::Blake2s256),
         _ => unreachable!(),
     };
 }
 
+fn consume_stdin() -> String {
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+
+    handle.read_to_string(&mut buffer).unwrap();
+
+    buffer
+}
+
+fn handle_stdin(input: &str) -> String {
+    if input == "-" {
+        consume_stdin()
+    } else {
+        input.to_string()
+    }
+}
+
 fn digest_command<D: Multihash>(input: &str, seq_mode: &str, verbose: bool, digester: D) {
     let value = match seq_mode {
-        "list" => serde_json::from_str::<Value<D>>(&input).unwrap(),
+        "list" => serde_json::from_str::<Value<D>>(&input).expect("Valid json"),
         "set" => serde_json::from_str::<ValueSet<D>>(&input)
-            .unwrap()
+            .expect("Valid json")
             .to_value(),
         _ => unreachable!(),
     };
